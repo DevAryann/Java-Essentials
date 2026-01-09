b681ae19e142d387d31d05617affe0b2f06dc726
DevAryann <wannacodearyan@gmail.com>
2026-01-09 14:24:20 +0530

    Added Example for Scanner

--- JavaEssentials/src/Scanner/Main.java
+++ JavaEssentials/src/Scanner/Main.java
@@ -1,22 +1,22 @@
 package Scanner;
-import java.util.Scanner;
+import java.util.Scanner;                // Importing utils (Scanner class )
 
 public class Main {
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);              // creating a new Scanner object
 
         System.out.print("What is your name : ");
 
-        String name = scanner.next();
+        String name = scanner.next();                          // Take the input as a String
 
         System.out.print("What is your age : ");
 
-        int age = scanner.nextInt();
+        int age = scanner.nextInt();                           // Take the input as a integer
 
         System.out.print("whats your gpa : ");
 
-        double gpa = scanner.nextDouble();
+        double gpa = scanner.nextDouble();                     // Take the input as a Double
 
         System.out.println("Your name is " + name);
 
@@ -25,7 +25,7 @@ public class Main {
         System.out.println("Your gpa is " + gpa);
 
 
-        scanner.close();
+        scanner.close();                                        // Closing the Scanner
 
     }
 }
