7677104ed9d08c3cdc14e7b7b68106cef41e54d9
DevAryann <wannacodearyan@gmail.com>
2026-01-11 22:59:41 +0530

    Added Example for Arrays

--- JavaEssentials/src/Arrays/Main.java
+++ JavaEssentials/src/Arrays/Main.java
@@ -3,7 +3,7 @@ package Arrays;
 public class Main {
 
         public static void main(String[] args) {
-            String[] fruits = {"Apple", "Mango", "Banana"};
+            String[] fruits = {"Apple", "Mango", "Banana"};             //Storing Strings in Array
 
             for (String fruit : fruits) {
                 System.out.println(fruit);
