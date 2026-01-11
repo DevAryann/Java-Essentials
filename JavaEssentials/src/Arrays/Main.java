3ee54bb2ff85a21aaac1bb07e6f68ff0b106cfa1
DevAryann <wannacodearyan@gmail.com>
2026-01-11 22:58:22 +0530

    Added Example for Arrays

---
+++ JavaEssentials/src/Arrays/Main.java
@@ -0,0 +1,14 @@
+package Arrays;
+
+public class Main {
+
+        public static void main(String[] args) {
+            String[] fruits = {"Apple", "Mango", "Banana"};
+
+            for (String fruit : fruits) {
+                System.out.println(fruit);
+            }
+        }
+    }
+
+
