b51dfd6c037e7f09a16822f8e2a13cf38ce0bc7e
DevAryann <wannacodearyan@gmail.com>
2026-01-20 22:31:08 +0530

    Example For File writing

---
+++ JavaEssentials/src/FileWriter/Main.java
@@ -0,0 +1,19 @@
+package FileWriter;
+
+import java.io.FileWriter;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String path = "text.txt";
+        String textContent = "Hi i am Aryan\nnice to meet you";
+
+        try (FileWriter file = new FileWriter(path)) {
+            file.write(textContent);
+            System.out.println("File Created successfully");
+        } catch (Exception e) {
+            System.out.println("Could not create file");
+
+        }
+    }
+}
