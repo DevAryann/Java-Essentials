2c02571c50eef5fa205ced1c156d82460780e965
DevAryann <wannacodearyan@gmail.com>
2026-01-23 14:36:04 +0530

    Example for File Reading(Read and display the file)

--- JavaEssentials/src/FileReader/Main.java
+++ JavaEssentials/src/FileReader/Main.java
@@ -9,6 +9,9 @@ public class Main {
         String file;
         try (BufferedReader reader = new BufferedReader(new FileReader(path))){  // Checks for the path
             System.out.println("File found");
+            while ((file = reader.readLine()) != null){                          // Loop to store content of txt line by line ("!= null" breaks the loop in end)
+                System.out.println(file);
+            }
         } catch (Exception e) {
             System.out.println("file not found");
         }
