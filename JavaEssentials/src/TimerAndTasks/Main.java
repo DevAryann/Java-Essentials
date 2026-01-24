c76d4c484107913124565c58f6ad2522eb25276f
DevAryann <wannacodearyan@gmail.com>
2026-01-24 17:41:40 +0530

    Example for TimerAndTaskes

---
+++ JavaEssentials/src/TimerAndTasks/Main.java
@@ -0,0 +1,31 @@
+package TimerAndTasks;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+
+
+        Timer timer = new Timer();
+        TimerTask task = new TimerTask() {
+            int count = 5 ;
+            @Override
+            public void run() {
+                System.out.println(count);
+                count--;
+
+                if (count == 0){
+                    System.out.println("Happy new year");
+                    timer.cancel();
+                }
+
+
+            }
+
+        };
+        timer.scheduleAtFixedRate(task,0,1000);
+    }
+}
